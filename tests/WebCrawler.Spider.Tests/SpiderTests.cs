cddad8ae99b467209fa4947e49a140bea3d565a6
Renan Ben Moshe <renan.moshe@gmail.com>
2018-06-12 23:03:29 -0300

    Unit tests getting credentials for Cloudant from vcap-local.json file

--- tests/WebCrawler.Spider.Tests/SpiderTests.cs
+++ tests/WebCrawler.Spider.Tests/SpiderTests.cs
@@ -1,32 +1,46 @@
+using System.IO;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebCrawler.CrossCutting;
 using WebCrawler.Domain.Interfaces;
 using WebCrawler.Repository;
 using WebCrawler.Spider.Application;
+using WebCrawler.Spider.Application.Interfaces;
 
 namespace WebCrawler.Spider.Tests
 {
     [TestClass]
     public class SpiderTests
     {
-        private readonly ISpiderRepository _spiderRepository;
+        private IConfigurationRoot Configuration { get; set; }
+
+        private readonly ISpiderAppService _spiderAppService;
 
         public SpiderTests()
         {
-            _spiderRepository = new SpiderRepository(new Credentials
-            {
-                host = "",
-                password = "",
-                username = ""
-            });
+            var credentials = GetCredentials();
+            var repository = new SpiderRepository(credentials);
+            _spiderAppService = new SpiderAppService(repository);
         }
 
         [TestMethod]
-        public void RodaTudo()
+        public void Crawler_Obtem_121_Urls_No_4Devs()
         {
-            var spiderAppService = new SpiderAppService(_spiderRepository);
+            Assert.AreEqual(121, _spiderAppService.Crawl("http://www.4devs.com.br/").Count);
+        }
 
-            Assert.AreEqual(121, spiderAppService.Crawl("http://www.4devs.com.br/").Count);
+        private Credentials GetCredentials()
+        {
+            var credentialsFilePath = Path.GetFullPath("../../../../../src/WebCrawler.Spider.Web");
+            var configBuilder = new ConfigurationBuilder().AddJsonFile(Path.Combine(credentialsFilePath, "vcap-local.json"), true);
+            Configuration = configBuilder.Build();
+
+            return new Credentials
+            {
+                host = Configuration["host"],
+                password = Configuration["password"],
+                username = Configuration["username"]
+            };
         }
     }
 }
